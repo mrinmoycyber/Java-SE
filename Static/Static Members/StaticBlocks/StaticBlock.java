775aaee1ed39a2a18212043c6cab85f2a276abf0
Mrinmoy Kar <73730091+mrinmoycyber@users.noreply.github.com>
2021-09-23 00:51:01 +0530

    Update StaticBlock.java

--- Static/Static Members/StaticBlocks/StaticBlock.java
+++ Static/Static Members/StaticBlocks/StaticBlock.java
@@ -5,8 +5,9 @@ public class StaticBlock {
 		System.out.println("Block1");
 	}
 
-	public static void main(String[] args) { // Nothing is inside main method...Nothing to execute but still Block 1 and Block 2 are printed because when we run the program this class
-												               // (StaticBlock) will get loaded and once the class gets loaded, It will execute the static blocks.
+	public static void main(String[] args) { // Nothing is inside main method...Nothing to execute but still Block 1 and Block 2 are printed 
+		                                 // because when we run the program this class
+                                                // (StaticBlock) will get loaded and once the class gets loaded, It will execute the static blocks.
 		}
 	static {
 		System.out.println("Block2");
