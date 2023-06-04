60efdd5ca378fc690d8de074da2841fe2ba94f71
Mrinmoy Kar <73730091+mrinmoycyber@users.noreply.github.com>
2023-06-04 22:17:20 +0530

    Update 1-D Array.java

--- Array/1D Array/1-D Array.java
+++ Array/1D Array/1-D Array.java
@@ -1,10 +1,13 @@
 package com.mrinu;
 
 public class OneDimesionalArray {
-
+        #Declaring an array
 	public static void main(String[] args) {
 		int A[] = new int[10];
 		int B[] = { 1, 2, 3, 4, 5 };
+		int C[];
+		C=new int[10];
+		int[] D=new int[5];
 
 		 //B[2] = 15;
 
