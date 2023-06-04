a317e03c30f822985558a4224a0ae5e0c3651f3b
Mrinmoy Kar <73730091+mrinmoycyber@users.noreply.github.com>
2023-06-04 23:25:16 +0530

    Update 1-D Array.java

--- Array/1D Array/1-D Array.java
+++ Array/1D Array/1-D Array.java
@@ -8,9 +8,9 @@ public class OneDimesionalArray {
 		int C[];
 		C=new int[10];
 		int[] D=new int[5];
-
+               #Changing the index 2 value in the array
 		 //B[2] = 15;
-
+              
 		// for(int i=0;i<A.length;i++) { System.out.println(A[i]); }
 
 		 //for(int i=0;i<B.length;i++) { System.out.println(B[i]); }
